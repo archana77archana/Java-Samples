1bd7399141633521dc28c6d879bcea044bcaaf49
Archana Chandrasekharan <kcarchana77@gmail.com>
2014-07-29 01:17:50 +0530

    Update DigitSum.java

--- DigitSum.java
+++ DigitSum.java
@@ -4,13 +4,13 @@ class DigitSum
 {
    public static void main(String args[])
    {
-       int i,a=0, sum=0;
-       i=Integer.parseInt(args[0]);           //taking integer from command line 
-       while(i!=0)
+       int i, a = 0, sum = 0;
+       i = Integer.parseInt(args[0]);           //entering values 
+       while(i != 0)
        {
-           a=i%10;
-           i=i/10;
-           sum=sum+a;
+           a = i % 10;
+           i = i / 10;
+           sum = sum + a;
        }
        System.out.println(sum);
    }
