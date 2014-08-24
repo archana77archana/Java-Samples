423b7f6d4e164e424770ca2f2ad542aedd503d6c
Archana Chandrasekharan <kcarchana77@gmail.com>
2014-08-24 14:31:35 +0530

    Create UseArgument.java

---
+++ UseArgument.java
@@ -0,0 +1,15 @@
+/*
+Author: Archana K C
+FileName: UseArgument.java
+*/
+
+public class UseArgument
+{
+  public static void main(String[] args)
+  {
+    System.out.print("Hi, ");
+    System.out.print(args[0]);
+    System.out.println(", How are you?");
+  }
+  
+}
