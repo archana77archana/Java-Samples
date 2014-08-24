19f4565cfc92a94945a5b8d6041b9c9fa14058a4
Archana Chandrasekharan <kcarchana77@gmail.com>
2014-08-24 14:12:56 +0530

    Create HelloWorld.java
    We break the process of programming in Java into three steps:
    
        1. Create the program by typing it into a text editor and saving it to a file named, say, HelloWorld.java.
        2. Compile it by typing "javac HelloWorld.java" in the terminal window.
        3. Run (or execute) it by typing "java HelloWorld" in the terminal window.
    
    The key line with System.out.println() send the text "Hello, World".
    
    To print a quotation mark, a newline, or a tab, use \", \n, or \t, respectively, within the quotation marks.

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+
+Author: Archana K C
+File name: HelloWorld.java
+
+*/
+
+public class HelloWorld
+{
+  public class void main(String[] args)
+  {
+    System.out.println("Hello World!");
+  }
+}
